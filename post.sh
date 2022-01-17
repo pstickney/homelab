c07a121edeed1e513fa0ed40b6a2f2e01030f32e
pstickney <770883+pstickney@users.noreply.github.com>
2022-01-17 12:39:28 -0500

    redirecting output to log file

--- post.sh
+++ post.sh
@@ -28,11 +28,11 @@ echo ""
 
 # Get Repos
 output "Add K8s Yum Repo"
-sudo yum-config-manager --add-repo https://raw.githubusercontent.com/pstickney/homelab/master/repos/kubernetes.repo
+sudo yum-config-manager --add-repo https://raw.githubusercontent.com/pstickney/homelab/master/repos/kubernetes.repo > "${LOG_FILE}" 2>&1
 result "$?"
 
 output "Add Docker Yum Repo"
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo >> "${LOG_FILE}" 2>&1
 result "$?"
 
 # Get requirements
@@ -46,27 +46,30 @@ echo ""
 set +e
 
 # Switch Docker cgroup driver to systemd
+output "Create /etc/docker/"
+sudo mkdir -p /etc/docker/ >> "${LOG_FILE}" 2>&1
+result "$?"
 output "Update Docker cgroup driver to systemd"
-sudo wget -O /etc/docker/daemon.json https://raw.githubusercontent.com/pstickney/homelab/master/config/docker-daemon.json
+sudo wget -O /etc/docker/daemon.json https://raw.githubusercontent.com/pstickney/homelab/master/config/docker-daemon.json >> "${LOG_FILE}" 2>&1
 result "$?"
 
 # Daemon Reload
 output "Daemon Reload"
-sudo systemctl daemon-reload
+sudo systemctl daemon-reload >> "${LOG_FILE}" 2>&1
 result "$?"
 
 # Docker
 output "Enable Docker"
-sudo systemctl enable docker
+sudo systemctl enable docker >> "${LOG_FILE}" 2>&1
 result "$?"
 output "Restart Docker"
-sudo systemctl restart docker
+sudo systemctl restart docker >> "${LOG_FILE}" 2>&1
 result "$?"
 
 # Kubelet
 output "Enable Kubelet"
-sudo systemctl enable kubelet
+sudo systemctl enable kubelet >> "${LOG_FILE}" 2>&1
 result "$?"
 output "Restart Kubelet"
-sudo systemctl restart kubelet
+sudo systemctl restart kubelet >> "${LOG_FILE}" 2>&1
 result "$?"
